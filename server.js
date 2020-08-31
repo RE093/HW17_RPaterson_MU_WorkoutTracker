493bfeeb690b50c65e0627595c6c3367397995cc
RE000 <re.paterson000@gmail.com>
2020-08-31 12:50:55 +1000

    exercise html path rendering

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
+var path = require("path");
 
 const app = express();
 
@@ -14,6 +15,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+// app.get("/api/workouts", (req, res) => {
+//     db.Workout.find({})
+//     .then(dbWorkout => {
+//         res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//         res.json(err);
+//     });
+// });
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+})
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);

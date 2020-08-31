450a5f91ac8e9c96a01b1e1e87a17a2ed83f20c9
RE000 <re.paterson000@gmail.com>
2020-08-31 14:23:09 +1000

    pushed seeds.js file to database, pushed database to /api/workouts route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
-var path = require("path");
+
+// const Workout = require("./models/Workouts");
 
 const app = express();
 
@@ -16,19 +18,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// app.get("/api/workouts", (req, res) => {
-//     db.Workout.find({})
-//     .then(dbWorkout => {
-//         res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//         res.json(err);
-//     });
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+// load exercise page
 app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/exercise.html"));
 })
+
+// create a new workout
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(workdb => {
+            res.json(workdb);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
